49a94b7a12b9f0a70dbe8968c6539ad3084575ce
Zach Carrera <zachcarrera@gmail.com>
2023-04-12 15:38:43 -0700

    spring project setup

---
+++ src/test/java/com/zachcarrera/safetravels/SaveTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachcarrera.safetravels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveTravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
